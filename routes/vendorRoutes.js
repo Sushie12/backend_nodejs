1a2bf75fc2f68f2a87d95139913161b04d9dda73
Sushie12 <srisushma.u@gmail.com>
2025-08-08 13:45:10 +0530

    unseen dupliactes in specifying routes. now handled

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -6,7 +6,7 @@ const router=express.Router();
 router.post('/register',vendorController.vendorRegister);
 router.post('/login',vendorController.vendorLogin);
 router.get('/all-vendors',vendorController.getAllVendors);
-router.get('/single-vendor/:id',vendorController.getVendorById)
+router.get('/single-vendor/:id',vendorController.getVendorById);
 
 module.exports=router;
 
